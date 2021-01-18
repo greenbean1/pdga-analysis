abdafe29a3e417945d95c9fec131dccd6990654b
greenbean1 <beauhoover1@gmail.com>
2021-01-18 10:20:38 -0800

    add main module

---
+++ main.py
@@ -0,0 +1,17 @@
+# Pull down JSON of 1 player statistics via Requests library
+# Use Requests library + Postman to pull player stats - https://requests.readthedocs.io/en/master/
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
