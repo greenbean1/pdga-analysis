393534cd543600dd3c197d033a026912b5d41c6f
greenbean1 <beauhoover1@gmail.com>
2021-01-18 10:48:07 -0800

    Readme updates and testing Requests library

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pdga-analysis
+# PDGA Analysis
+This is a data analysis project exploring PDGA player information
+
+## Next Goal: Pull JSON of 1 player information
+1. Successfully login (POST request) - hardcode credentials
+2. Successful Player statistics on 139870
+3. Read in Beau's username and password off of a local file (set up gitignore)
+
+## Things I Learned (or at least started learning)
+1. [HTTP Status Codes](https://www.restapitutorial.com/httpstatuscodes.html)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # Pull down JSON of 1 player statistics via Requests library
 # Use Requests library + Postman to pull player stats - https://requests.readthedocs.io/en/master/
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import requests
 
 
 def print_hi(name):
@@ -10,8 +11,15 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+r = requests.get('https://api.github.com/events')
+
+#POST request w/URL & Body (contains username & password)
+#
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(r)
+    print('heyy')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
