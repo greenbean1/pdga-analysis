53d0f6d0e667a8cf72d4fc31a6c8001ddb965211
greenbean1 <beauhoover1@gmail.com>
2021-01-20 13:09:41 -0800

    Fixed API call to get player stats

--- main.py
+++ main.py
@@ -3,39 +3,43 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import requests
+from http import HTTPStatus
 import json
+import requests
+from typing import Dict, Tuple
 
 PDGA_API_LOGIN_URL = 'https://api.pdga.com/services/json/user/login'
 
 
-def get_pdga_login():  # -> Dict[str, str]:
+def get_pdga_login() -> Dict[str, str]:
     print('Getting PDGA login info')
     with open('pdga_info.txt', 'r') as pc:
         info = pc.read().splitlines()
     return {'username': info[0], 'password': info[1]}
 
 
-def get_pdga_sessid() -> str:
+def get_pdga_sessid() -> Tuple[str, str]:
     print('Getting PDGA sessid')
     request_login = requests.post(PDGA_API_LOGIN_URL, data=get_pdga_login())
-    login_response = request_login.text
+    login_response = request_login.text  # Refactor here: requests_login.json
     login_response_dict = json.loads(login_response)
-    return login_response_dict["sessid"]
+    print(login_response_dict)
+    return login_response_dict["session_name"], login_response_dict["sessid"]
 
 
 if __name__ == '__main__':
-    sessid = get_pdga_sessid()
+    session_name, sessid = get_pdga_sessid()
     print(sessid)
     url = 'https://api.pdga.com/services/json/player-statistics?pdga_number=37817'
-    cookie = dict(session_name='iM-aNzJdNUFXrOVdL_H4pstXiViIA9MqyBMMBcVbmxY')
-    cookies = dict(session_name=sessid)
-    print(cookie)
-    player_info_eagle = requests.get(url, cookies=cookies)
+    player_info_eagle = requests.get(url, cookies={session_name: sessid})
     print(player_info_eagle)
-    print(player_info_eagle.content)
-    print(player_info_eagle.text)
-    print(player_info_eagle.json)
+    if player_info_eagle.status_code != HTTPStatus.OK:
+        print(f'Failed to get player stats: status {player_info_eagle.status_code}')
+        #  Add break/return
+    else:  # Won't need else when in a function
+        print('yay success getting player stats')
+        player_info = player_info_eagle.json()
+        print(player_info.get('players')[0])  # look into DotDict; lazy since could cause crash
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
