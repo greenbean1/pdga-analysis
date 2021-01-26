56672628a2c5f0e5601ce382b18048662e8f0cbe
greenbean1 <beauhoover1@gmail.com>
2021-01-25 17:43:43 -0800

    Create csv functions module

---
+++ csv_functions.py
@@ -0,0 +1,31 @@
+"""
+This module has CSV functions
+"""
+
+import csv
+import os
+from typing import Dict
+
+
+PLAYER_STATS_CSV = 'player_stats.csv'
+
+
+def delete_player_stats_csv():
+    try:
+        os.remove(PLAYER_STATS_CSV)
+        print('Successfully deleted player stats csv')
+    except OSError:
+        print('No player stats file to delete')
+        pass
+
+
+def dict_to_csv(player_dict: Dict[str, str]) -> None:
+    delete_player_stats_csv()
+    field_names = player_dict.keys()
+    with open('player_stats.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(field_names)
+        player_values = []
+        for value in player_dict.values():
+            player_values.append(value)
+        writer.writerow(player_values)
