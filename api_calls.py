a13f649f23f86486f98e2ebc7169c71f69b1c47c
greenbean1 <beauhoover1@gmail.com>
2021-01-26 14:24:42 -0800

    Update api calls to update API credentials when need to log in to API again

--- api_calls.py
+++ api_calls.py
@@ -18,6 +18,17 @@ def _get_pdga_login() -> Dict[str, str]:
     return {'username': info[0], 'password': info[1]}
 
 
+# Haven't actually tested this
+def update_pdga_api_session_info_file(session_info: Dict[str, str]) -> None:
+    print('Setting PDGA API session info')
+    with open('old_pdga_api_info.txt', 'w') as pc:
+        pc.truncate()
+        pc.write(session_info['session_name'])
+        pc.write('\n')
+        pc.write(session_info['sessid'])
+
+
+#  Returns session_name and sessid of new PDGA API login
 def _get_new_pdga_session_info() -> Dict[str, str]:
     print('Getting new PDGA API session info')
     request_login = requests.post(API_LOGIN_URL, data=_get_pdga_login())
@@ -25,7 +36,8 @@ def _get_new_pdga_session_info() -> Dict[str, str]:
     login_response_dict = json.loads(login_response)
     print('API Login Response dictionary:')
     print(login_response_dict)
-    return {'session_name': login_response_dict["session_name"], 'sessid': login_response_dict["sessid"]}
+    update_pdga_api_session_info_file(login_response_dict)
+    return {'session_name': login_response_dict['session_name'], 'sessid': login_response_dict['sessid']}
 
 
 def _get_old_pdga_session_info() -> Dict[str, str]:
@@ -35,8 +47,7 @@ def _get_old_pdga_session_info() -> Dict[str, str]:
     return {'session_name': info[0], 'sessid': info[1]}
 
 
-# Make me dumb! Return JSON or fact that it failed (Throw my own error) - Try and Except in calling context
-def get_player_stats(session_name: str, sessid: str):
+def get_player_stats(session_name: str, sessid: str) -> Dict[str, str]:
     print('Getting player info')
     player_info_eagle = requests.get(PLAYER_STATS_URL, cookies={session_name: sessid})
     if player_info_eagle.status_code != HTTPStatus.OK:
@@ -45,15 +56,16 @@ def get_player_stats(session_name: str, sessid: str):
     else:
         print('Got player info Eagle')
         print(player_info_eagle)
-        return player_info_eagle.json().get('players')[0] # look into DotDict; lazy since could cause crash
+        return player_info_eagle.json().get('players')[0]  # look into DotDict; lazy since could cause crash
 
 
-def get_eagle_stats():
+def get_eagle_stats() -> Dict[str, str]:
     try:
         print('Trying old PDGA session info')
         old_pdga_session_info = _get_old_pdga_session_info()
         return get_player_stats(old_pdga_session_info['session_name'], old_pdga_session_info['sessid'])
     except Exception:  # Ask about correct way to do this!
         print('Trying new PDGA session info')
+        # Set txt file credentials!!
         new_pdga_session_info = _get_new_pdga_session_info()
         return get_player_stats(new_pdga_session_info['session_name'], new_pdga_session_info['sessid'])
