2b291ccf27ab639508ef0f18a82434ac86195a0b
greenbean1 <beauhoover1@gmail.com>
2021-01-25 17:44:31 -0800

    Refactor api calls and minor edits to main

--- api_calls.py
+++ api_calls.py
@@ -5,7 +5,7 @@ This module has the PDGA API calls
 from http import HTTPStatus
 import json
 import requests
-from typing import Dict, Tuple
+from typing import Dict
 
 API_LOGIN_URL = 'https://api.pdga.com/services/json/user/login'
 PLAYER_STATS_URL = 'https://api.pdga.com/services/json/player-statistics?pdga_number=37817'
@@ -18,13 +18,14 @@ def _get_pdga_login() -> Dict[str, str]:
     return {'username': info[0], 'password': info[1]}
 
 
-def _get_new_pdga_session_info() -> Tuple[str, str]:
+def _get_new_pdga_session_info() -> Dict[str, str]:
     print('Getting new PDGA API session info')
     request_login = requests.post(API_LOGIN_URL, data=_get_pdga_login())
     login_response = request_login.text  # Refactor here: requests_login.json
     login_response_dict = json.loads(login_response)
+    print('API Login Response dictionary:')
     print(login_response_dict)
-    return login_response_dict["session_name"], login_response_dict["sessid"]
+    return {'session_name': login_response_dict["session_name"], 'sessid': login_response_dict["sessid"]}
 
 
 def _get_old_pdga_session_info() -> Dict[str, str]:
@@ -33,22 +34,26 @@ def _get_old_pdga_session_info() -> Dict[str, str]:
         info = pc.read().splitlines()
     return {'session_name': info[0], 'sessid': info[1]}
 
-# Make me dumb! Take session_name & sessid and return JSON or fact that it failed (Throw my own error) - Try and Except in calling context
-def get_player_info() -> Dict:
-    try:
-        old_session_info = _get_old_pdga_session_info()
-        session_name, sessid = old_session_info['session_name'], old_session_info['sessid']
-        player_info_eagle = requests.get(PLAYER_STATS_URL, cookies={session_name: sessid})
-    session_name, sessid = _get_pdga_session_info()
 
-    print(player_info_eagle)
+# Make me dumb! Return JSON or fact that it failed (Throw my own error) - Try and Except in calling context
+def get_player_stats(session_name: str, sessid: str):
+    print('Getting player info')
+    player_info_eagle = requests.get(PLAYER_STATS_URL, cookies={session_name: sessid})
     if player_info_eagle.status_code != HTTPStatus.OK:
         print(f'Failed to get player stats: status {player_info_eagle.status_code}')
-        #  Add break/return
-    else:  # Won't need else when in a function
-        print('yay success getting player stats')
-        player_info = player_info_eagle.json()
-        print(f'Player info type = {type(player_info)}')
-        print(player_info)
-        print(player_info.get('players')[0])  # look into DotDict; lazy since could cause crash
-    return {'test_key': 'test_value'}
+        raise Exception('Failed to get player stats')
+    else:
+        print('Got player info Eagle')
+        print(player_info_eagle)
+        return player_info_eagle.json().get('players')[0] # look into DotDict; lazy since could cause crash
+
+
+def get_eagle_stats():
+    try:
+        print('Trying old PDGA session info')
+        old_pdga_session_info = _get_old_pdga_session_info()
+        return get_player_stats(old_pdga_session_info['session_name'], old_pdga_session_info['sessid'])
+    except Exception:  # Ask about correct way to do this!
+        print('Trying new PDGA session info')
+        new_pdga_session_info = _get_new_pdga_session_info()
+        return get_player_stats(new_pdga_session_info['session_name'], new_pdga_session_info['sessid'])
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ This is where the main magic happens and it all comes together
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import api_calls
+import csv_functions
 
 if __name__ == '__main__':
-    player_info = api_calls.get_player_info()
-    print(player_info)
+    player_info = api_calls.get_eagle_stats()  # player_info is a dictionary
+    # print(type(player_info))
+    # print(player_info)
+    csv_functions.dict_to_csv(player_info)
